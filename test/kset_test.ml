3e8ec76f003acfb40924847668f9826ec0618388
ergl <ergl@users.noreply.github.com>
2017-02-24 17:53:45 +0100

    Add prev/next_key tests

--- test/kset_test.ml
+++ test/kset_test.ml
@@ -59,10 +59,46 @@ let subkeys_subset_test =
       List.for_all (fun k -> List.mem k all_subkeys) tail
   )
 
+let prev_key_test =
+  let open QCheck in
+  Test.make
+    ~name: "Prev key on hierarchies returns the first superkey"
+    ~count: 1000
+    Models.arbitrary_key_hierarchy (
+    fun key_h ->
+      let st = Kset.empty ()
+      and n =
+        Random.self_init ();
+        (Random.int @@ List.length key_h - 1) + 1
+      in
+      let elt = List.nth key_h n in
+      let _ = List.iter (fun k -> Kset.add k st) key_h in
+      Kset.prev_key elt st = Some (List.nth key_h @@ n - 1)
+  )
+
+let next_key_test =
+  let open QCheck in
+  Test.make
+    ~name: "Next key on hierarchies returns the first subkey"
+    ~count: 1000
+    Models.arbitrary_key_hierarchy (
+    fun key_h ->
+      let st = Kset.empty ()
+      and n =
+        Random.self_init ();
+        Random.int @@ List.length key_h - 1
+      in
+      let elt = List.nth key_h n in
+      let _ = List.iter (fun k -> Kset.add k st) key_h in
+      Kset.next_key elt st = Some (List.nth key_h @@ n + 1)
+  )
+
 let _ =
   QCheck_runner.run_tests_main [
     positive_find_test
   ; negative_member_test
   ; subkeys_bare_test
   ; subkeys_subset_test
+  ; prev_key_test
+  ; next_key_test
   ]
