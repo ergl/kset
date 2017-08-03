4192094fe7b5479f4d741f2c8f553756f94b633a
ergl <ergl@users.noreply.github.com>
2017-08-03 15:15:12 +0200

    Don't mangle numbers

--- src/encoding.ml
+++ src/encoding.ml
@@ -3,27 +3,11 @@ type t = string
 let d_string x = x
 
 let d_float fl =
-  let open Nativeint in
-  let (<<) = Nativeint.shift_left in
-  let
-    a = fl << 56 |> to_string and
-    b = fl << 48 |> to_string and
-    c = fl << 40 |> to_string and
-    d = fl << 32 |> to_string and
-    e = fl << 24 |> to_string and
-    f = fl << 16 |> to_string and
-    g = fl << 8 |> to_string and
-    h = fl |> to_string
-  in
-  d_string @@ a ^ b ^ c ^ d ^ e ^ f ^ g ^ h
+  Nativeint.to_float fl
+  |> string_of_float
+  |> d_string
 
 let d_int x =
-  let open Int32 in
-  let (<<) = Int32.shift_left in
-  let
-    a = x << 24 |> to_string and
-    b = x << 16 |> to_string and
-    c = x << 8 |> to_string and
-    d = x |> to_string
-  in
-  d_string @@ a ^ b ^ c ^ d
+  Int32.to_int x
+  |> string_of_int
+  |> d_string
